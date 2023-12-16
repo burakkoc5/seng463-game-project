b772fdf06329434ea5cc45c6fbc82488476678f7
Furkan Safa Altunyuva <furkansafaaltunyuva@gmail.com>
2023-12-16 11:15:54 +0300

    Added TODO and Adjusted Colliders for Stick

--- Assets/Basketball Minigame/BasketballMinigameManager.cs
+++ Assets/Basketball Minigame/BasketballMinigameManager.cs
@@ -10,7 +10,6 @@ public class BasketballMinigameManager : MonoBehaviour
     IEnumerator InstantiateNewBall()
     {
         yield return new WaitForSeconds(2f);
-        
         GameObject instantiatedBasketball = Instantiate(basketballPrefab);
         basketballRigidbody = instantiatedBasketball.GetComponent<Rigidbody>();
         stick.GetComponent<Stick>().enabled = true;
@@ -19,6 +18,7 @@ public class BasketballMinigameManager : MonoBehaviour
     
     public void throwBall(float x, float y, float z)
     {
+        //TODO: Randomize X
         basketballRigidbody.AddForce(new Vector3(x, y, z), ForceMode.Impulse);
         StartCoroutine(InstantiateNewBall());
     }
